e710c9903f2d735eb219cc8a28042f81d2667b6b
Siddhi Vinayaka <siddhi.vinayaka@bhge.com>
2020-06-01 16:56:52 +0530

    update file

---
+++ public/scripts/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var template = React.createElement(
+  'p',
+  null,
+  'This is JSX learning part!'
+);
+var appRoot = document.getElementById('root');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,4 @@
+var template = <p>This is JSX learning part!</p>;
+var appRoot = document.getElementById('root');
+
+ReactDOM.render(template, appRoot);
